08340bd4d9c49eefa2b5f7c8b1f3bb30d288861e
eliduty <eliduty@qq.com>
2021-12-22 14:09:55 +0800

    feat: 增加vue3配置模板

--- src/config/fes.config.js
+++ src/config/fes.config.js
@@ -27,6 +27,19 @@ module.exports = {
       },
       repository: 'https://github.com/eliduty/fes-config.git#vue3',
     },
+    'vue3-ts': {
+      dependencies: {
+        husky:['husky@4.3.8'],
+        'lint-staged': ['lint-staged'],
+        eslint: ['eslint', 'eslint-plugin-vue', 'eslint-plugin-vue', 'eslint-plugin-prettier', 'eslint-config-prettier'],
+        prettier: ['prettier'],
+        stylelint: ['stylelint', 'stylelint-config-standard', 'stylelint-config-rational-order', 'stylelint-order'],
+        commitlint: ['@commitlint/cli', '@commitlint/config-conventional'],
+        changelog: ['standard-version'],
+        'ls-lint':['@ls-lint/ls-lint']
+      },
+      repository: 'https://github.com/eliduty/fes-config.git#vue3-ts',
+    }
   },
   // fes create 项目配置模板
   projectTemplate: {
